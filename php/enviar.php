f000c01cfdeca94eda26c242a12f50b2c586d1cd
David Arias Escribano <91081693+DavidAr15@users.noreply.github.com>
2022-11-27 11:42:22 +0100

    subida pagina

---
+++ php/enviar.php
@@ -0,0 +1,11 @@
+﻿<?php 
+	$nombre = $_POST['nombre'];
+	$email = $_POST['email'];
+	$asunto = 'Formulario Rellenado';
+	$mensaje = "Nombre: ".$nombre."<br> Email: $email<br> Mensaje:".$_POST['mensaje'];
+
+
+	if(mail('davidaresc@outlook.es', $asunto, $mensaje)){
+		echo "Correo enviado";
+	}
+ ?>
